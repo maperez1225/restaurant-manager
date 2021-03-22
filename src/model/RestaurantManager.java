52a03e06ea6fe1a1650f9c9f3870ecefb90d3cf0
Martin Perez Lopez <martinperez_lopez@hotmail.com>
2021-03-22 15:38:55 -0500

    Defined add methods for model

--- src/model/RestaurantManager.java
+++ src/model/RestaurantManager.java
@@ -15,4 +15,22 @@ public class RestaurantManager{
 		customers = new ArrayList<Customer>();
 		orders = new ArrayList<Order>();
 	}
+	public void addUser(User user) {
+		users.add(user);
+	}
+	public void addIngredient(Ingredient ingredient) {
+		ingredients.add(ingredient);
+	}
+	public void addProduct(Product product) {
+		products.add(product);
+	}
+	public void addType(ProductType type) {
+		types.add(type);
+	}
+	public void addCustomer(Customer customer) {
+		customers.add(customer);
+	}
+	public void addOrder(Order order) {
+		orders.add(order);
+	}
 }
